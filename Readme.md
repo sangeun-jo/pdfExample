70645d288ad207551a459819db35c93c28fecead
sangeun-jo <sangeun.jo@outlook.kr>
2022-06-14 08:50:51 -0700

    create pdf

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### pdf 연습 
+
+**참고**  
+* 읽고 쓰기 예제: https://www.csharpstudy.com/Practical/Prac-pdf.aspx
+* pdf 뷰어 만들기: https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=goldrushing&logNo=220958998997
---
+++ pdf_example/create_doc.cs
@@ -0,0 +1,22 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf; 
+
+Document doc = new Document(iTextSharp.text.PageSize.LETTER);
+
+//pdf 생성 위치 : 프로젝트 경로\bin\Debug\net6.0
+PdfWriter wr = PdfWriter.GetInstance(doc, new FileStream("simple.pdf", FileMode.Create));
+
+doc.Open();
+
+doc.AddTitle("Simple PDF 생성 예제");
+doc.AddAuthor("Kelly");
+doc.AddCreationDate();
+
+doc.Add(new Paragraph("It's been a loooooong time"));
+
+//string batangttf = System.IO.Path.Combine(Environment.GetEnvironmentVariable("SystemRoot"), @"Fonts\BatangChe.TTF");
+
+doc.Close();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Doc file Created!");
